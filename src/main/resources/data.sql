3eb0c13aabdde71c8c7f7996f429c74f59bd7bb3
FootlockerDE <jan.ruegge@gmail.com>
2023-12-23 17:01:37 +0100

    0.0.3 Added login - switched from H2 in memory DB to PostgreSQL
    TODO:
    - refine database schema
    - finish all models  and repositories

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,8 @@
-INSERT INTO drink (name, is_alcoholic, purchase_price, sale_price, available_quantity, img_url,category_id) VALUES ('Cool Lemonade', false, 1.50, 2.99, 100, 'http://example.com/lemonade.jpg',1);
-INSERT INTO drink (name, is_alcoholic, purchase_price, sale_price, available_quantity, img_url, category_id) VALUES ('Iced Tea', false, 1.00, 2.50, 150, 'http://example.com/icedtea.jpg',2);
-INSERT INTO category (name) VALUES ( 'Soda' );
-INSERT INTO category (name) VALUES ( 'Bier' );
-INSERT INTO sale ( drink_id, quantity, sale_date) VALUES (  2, 4, '2023-12-20' );
-INSERT INTO sale (drink_id, quantity, sale_date) VALUES ( 1, 10, '2023-12-24' );
+INSERT INTO categories (name) VALUES ( 'Soda' );
+INSERT INTO categories (name) VALUES ( 'Bier' );
+INSERT INTO drinks (name, is_alcoholic, purchase_price, sale_price, available_quantity, img_url,category_id) VALUES ('Cool Lemonade', false, 1.50, 2.99, 100, 'http://example.com/lemonade.jpg',1);
+INSERT INTO drinks (name, is_alcoholic, purchase_price, sale_price, available_quantity, img_url, category_id) VALUES ('Iced Tea', false, 1.00, 2.50, 150, 'http://example.com/icedtea.jpg',2);
 
-//TODO Refactor drink to product
-//TODO add sample data for Purchase and user
+
+-- TODO Refactor drink to product
+-- TODO add sample data for Purchase and user
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,31 +1,31 @@
-create table if not exists drink(
-                      product_id int auto_increment,
-                      name varchar(255) NOT NULL,
-                      is_alcoholic boolean NOT NULL,
-                      purchase_price double NOT NULL,
-                      sale_price double NOT NULL,
-                      available_quantity int NOT NULL,
-                      img_url varchar(255),
-                      category_id int NOT NULL
-);
-
-create table if not exists category(
-    category_id int auto_increment NOT NULL,
-    name varchar(255) NOT NULL
-);
-
-create table if not exists sale(
-    sale_id int auto_increment,
-    drink_id int,
-    quantity int,
-    sale_date date
-);
-
-
-create table if not exists users(
-    user_id int auto_increment,
-    username varchar(255),
-    password varchar(255),
-    roles varchar(255)
-);
-/* TODO add table for data, purchase, purchaseItem, saleItem, USer */
+-- create table if not exists drink(
+--                       product_id int auto_increment,
+--                       name varchar(255) NOT NULL,
+--                       is_alcoholic boolean NOT NULL,
+--                       purchase_price double NOT NULL,
+--                       sale_price double NOT NULL,
+--                       available_quantity int NOT NULL,
+--                       img_url varchar(255),
+--                       category_id int NOT NULL
+-- );
+--
+-- create table if not exists category(
+--     category_id int auto_increment NOT NULL,
+--     name varchar(255) NOT NULL
+-- );
+--
+-- create table if not exists sale(
+--     sale_id int auto_increment,
+--     drink_id int,
+--     quantity int,
+--     sale_date date
+-- );
+--
+--
+-- create table if not exists users(
+--     user_id int auto_increment,
+--     username varchar(255),
+--     password varchar(255),
+--     roles varchar(255)
+-- );
+-- /* TODO add table for data, purchase, purchaseItem, saleItem, USer */
